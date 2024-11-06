c51fb4cdbeb60492ee6081f65d18b934823d3a59
lpenge <111988836+lpenge@users.noreply.github.com>
2024-11-06 13:53:07 +0000

    deleted:    __pycache__/app.cpython-312.pyc 	deleted:    app.py 	new file:   index.html 	deleted:    static/styles.css 	new file:   styles.css 	deleted:    templates/index.html 	deleted:    templates/layout.html

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, flash, redirect, render_template, request, session
-
-# Configure application
-app = Flask(__name__)
-
-
-@app.route("/")
-def main():
-    return render_template("index.html")
