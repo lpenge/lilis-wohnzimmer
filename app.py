b624428b9fe3aa13ac2816e043566be03c1b513e
lpenge <111988836+lpenge@users.noreply.github.com>
2024-11-06 13:38:25 +0000

    modified:   __pycache__/app.cpython-312.pyc 	modified:   app.py 	renamed:    Templates/index.html -> templates/index.html 	renamed:    Templates/layout.html -> templates/layout.html

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, flash, redirect, render_template, request, session
 
 # Configure application
 app = Flask(__name__)
+
+
 @app.route("/")
 def main():
-    return render_template("/workspaces/lilis-wohnzimmer/Templates/index.html")
+    return render_template("index.html")
